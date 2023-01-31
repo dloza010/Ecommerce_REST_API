0febfbd7d0260ee7bcb6e301b545e10c02d91bb3
dloza010 <david.loza010@gmail.com>
2023-01-31 17:33:41 -0500

    created product model

---
+++ models/product.js
@@ -0,0 +1,65 @@
+const db = require('../db')
+const pgb = require('pg-promise')({capSQL: true})
+
+module.exports = class productModel{
+
+    /**
+     * List all product
+     * @param {Object} options [Query options]
+     * @return {Array}         [Array of products]
+     */
+
+     async getProducts(options = {}){
+
+        try{
+
+            //Generate SQL statement
+        const statement =  `SELECT * FROM products`;
+        const values = [];
+
+        //Execute SQL statement
+        const results = await db.query(statement, values);
+
+        if (results.rows?.length){
+            return results.rows;
+        }
+
+        return [];
+
+        }catch(err){
+            throw new Error(err);
+        }
+        
+    }
+
+    /**
+     * List product by id
+     * @param {Object}          id [Product id]
+     * @return {Object|Null}       [Product record]
+     */
+
+    async getById(id){
+
+        try{
+
+            //Generate SQL statement
+            const statement = `SELECT * FROM products
+            WHERE id = $1`
+            const values = id;
+
+            //Execute SQL statement
+            const results = await db.query(statement, values);
+
+            if(results.rows?.length){
+                return results.rows[0];
+            }
+
+            return null;
+
+        }catch(err){
+            throw new Error(err);
+        }
+    }
+
+}
+    
